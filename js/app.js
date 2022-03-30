1dd4dabc1fcb5bcfbeaf93bdc53445d70e38404c
Brandon Aldred <brandonaldred@gmail.com>
2022-03-30 12:55:21 -0700

    adding-pages

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
-for(let i = 0; i < posts.length; i++) {
-    let post = posts[i];
-    buildPost(post, users[post.id - 1])
+function buildFeed(id) {
+    for(let i = 0; i < posts.length; i++) {
+        let post = posts[i];
+        buildPost(post, users[post.id - 1])
+    }
 }
+
+
+
+const navHome = document.querySelector('.nav-home');
+navHome.addEventListener('click', () => {
+    document.querySelector('.feed-container').innerHTML = '';
+    buildFeed();
+});
+
+
+buildFeed();
